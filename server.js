dca85eec5c09151d81f5cb7b880bb13d314f7c12
LinusPiorkowsky <s_piorkowsky23@stud.hwr-berlin.de>
2025-08-26 15:22:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -607,6 +607,24 @@ app.get('/api/stats', authMiddleware, requireRole(['manager', 'admin']), async (
   }
 });
 
+// Serve landing page at root
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'landing.html'));
+});
+
+// Serve app at /app or /login
+app.get('/app', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 // Serve frontend
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
